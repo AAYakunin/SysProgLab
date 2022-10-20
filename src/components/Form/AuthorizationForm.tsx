796a18d799613e9238ceaf5929a11c53b0719ff1
YAkuninAA <YAkuninAA@DOM201.LOCAL>
2022-10-20 10:21:21 +0700

    init

---
+++ src/components/Form/AuthorizationForm.tsx
@@ -0,0 +1,63 @@
+import React, {FormEvent, useState} from 'react';
+import './Form.css';
+
+
+export default function AuthorizationForm() {
+    const [login, setLogin] = useState("");
+    const [loginError, setLoginError] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+
+    const isValid = (): boolean => {
+        let result = true;
+        setLoginError("");
+        setPasswordError("");
+        if (login.length === 0) {
+            setLoginError("Логин не может быть пустым");
+            result = false;
+        }
+        if (!/^([a-z0-9]{6,20})$/.test(login)) {
+            setLoginError("Логин должен содержать от 6 до 20 символов латинского алфавита и цифры.");
+            result = false;
+        }
+        if (password.length === 0) {
+            setPasswordError("Пароль не может быть пустым");
+            result = false;
+        }
+        return result;
+    };
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if(isValid()) {
+
+        }
+    };
+
+    return <>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Логин
+                    <input value={login}
+                           onChange={e => setLogin(e.target.value)}
+                    />
+                </label>
+                {loginError && <div className="error">
+                    {loginError}
+                </div>}
+            </div>
+            <div>
+                <label>Пароль.
+                    <input value={password}
+                           type={"password"}
+                           onChange={e => setPassword(e.target.value)}
+                    />
+                </label>
+                {passwordError && <div className="error">
+                    {passwordError}
+                </div>}
+            </div>
+            <button type={"submit"}>Регистрация</button>
+        </form>
+    </>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RegistrationForm from "./components/Form/RegistrationForm";
+import AuthorizationForm from './components/Form/AuthorizationForm';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RegistrationForm />
+    <AuthorizationForm />
   </React.StrictMode>
 );
 
